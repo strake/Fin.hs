7440cbba602fe66b8096297af35f06ca2342bb05
M Farkas-Dyck <strake888@gmail.com>
2020-01-23 09:23:54 -0800

    define `pred`

--- Data/Fin.hs
+++ Data/Fin.hs
@@ -1,3 +1,11 @@
-module Data.Fin (Fin (..), enum, inj₁, lift₁, fromFin, toFin, toFinMay) where
+module Data.Fin (Fin (..), enum, inj₁, lift₁, fromFin, toFin, toFinMay, pred) where
 
+import Prelude ()
+import Data.Maybe (Maybe (..))
 import Data.Fin.Private
+import qualified Data.Peano as P
+
+pred :: Fin (P.Succ n) -> Maybe (Fin n)
+pred = \ case
+    Zero -> Nothing
+    Succ n -> Just n

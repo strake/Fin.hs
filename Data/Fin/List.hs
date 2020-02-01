e9232c2144e3f4e6326e9bc1c342c75d3c5781a6
M Farkas-Dyck <strake888@gmail.com>
2020-02-01 12:40:24 -0800

    findIndex

--- Data/Fin/List.hs
+++ Data/Fin/List.hs
@@ -1,6 +1,14 @@
 module Data.Fin.List (Peano, List (..),
-                      fromList, uncons, head, tail, init, last, reverse, rotate, at, swap, (!!)) where
+                      fromList, uncons, head, tail, init, last, reverse, rotate, at, swap, (!!), findIndex) where
 
 import Prelude ()
-import Data.Fin.Private
-import Data.Peano
+import Data.Bool (Bool, bool)
+import Control.Applicative
+import Control.Category (id)
+import Data.Fin.Private as Fin
+import Data.Peano (Peano)
+
+findIndex :: Alternative p => (a -> Bool) -> List n a -> p (Fin n)
+findIndex p = \ case
+    Nil -> empty
+    a:.as -> bool id (pure Zero <|>) (p a) (Succ <$> findIndex p as)
